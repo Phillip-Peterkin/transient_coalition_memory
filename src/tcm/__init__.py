0a69cb091e8cfec9ab527b0bb3f5b1884df993f8
Phillip Peterkin <44818722+Phillip-Peterkin@users.noreply.github.com>
2026-07-21 14:18:40 -0700

    Add frozen TCM reference package

---
+++ src/tcm/__init__.py
@@ -0,0 +1,5 @@
+"""Public access to the frozen Transient Coalition Memory reference model."""
+
+from .reference import BatchedReserveCellular, FairProvGraph
+
+__all__ = ["BatchedReserveCellular", "FairProvGraph"]
